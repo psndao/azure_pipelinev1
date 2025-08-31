06267a64a2ca1ceffe31752347990783df47bf93
psndao <papesemoundao2016@gmail.com>
2025-08-31 20:28:44 +0200

    fix(terraform): replace() regex + numeric

--- infra/main.tf
+++ infra/main.tf
@@ -1,14 +1,15 @@
-﻿resource "random_string" "suffix" {
-  length = 4
-  lower  = true
-  upper  = false
-  number = true
-  special= false
+resource "random_string" "suffix" {
+  length  = 4
+  lower   = true
+  upper   = false
+  numeric = true
+  special = false
 }
 
 locals {
-  project_norm   = lower(regexreplace(var.project, "[^a-z0-9]", ""))
-  sa_prefix      = substr("st${local.project_norm}dev", 0, 20)
+  # normalise: minuscule + supprime tout ce qui n'est pas [a-z0-9]
+  project_norm   = lower(replace(var.project, "[^a-z0-9]", ""))
+  sa_prefix      = substr("st${local.project_norm}dev", 0, 20) # garde de la place pour le suffixe
   sa_name        = "${local.sa_prefix}${random_string.suffix.result}"
   rg_name        = "rg-${var.project}-dev"
   container_name = "data"
@@ -21,14 +22,14 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "sa" {
-  name                              = local.sa_name
-  resource_group_name               = azurerm_resource_group.rg.name
-  location                          = azurerm_resource_group.rg.location
-  account_tier                      = "Standard"
-  account_replication_type          = "LRS"
-  allow_nested_items_to_be_public   = false
-  min_tls_version                   = "TLS1_2"
-  tags                              = var.tags
+  name                            = local.sa_name
+  resource_group_name             = azurerm_resource_group.rg.name
+  location                        = azurerm_resource_group.rg.location
+  account_tier                    = "Standard"
+  account_replication_type        = "LRS"
+  allow_nested_items_to_be_public = false
+  min_tls_version                 = "TLS1_2"
+  tags                            = var.tags
 }
 
 resource "azurerm_storage_container" "container" {
