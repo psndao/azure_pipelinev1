c4cd9c8d1ebb58f5b3fc2427d00493dd152d01bf
psndao <papesemoundao2016@gmail.com>
2025-08-31 20:17:01 +0200

    fix(terraform): syntaxe HCL + encodage sans BOM

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,3 +1,11 @@
-﻿output "resource_group_name"  { value = azurerm_resource_group.rg.name }
-output "storage_account_name" { value = azurerm_storage_account.sa.name }
-output "container_name"       { value = azurerm_storage_container.container.name }
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.sa.name
+}
+
+output "container_name" {
+  value = azurerm_storage_container.container.name
+}
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,3 +1,20 @@
-﻿variable "project"  { description = "Nom court du projet"; type = string; default = "demo" }
-variable "location" { description = "Région Azure";       type = string; default = "westeurope" }
-variable "tags"     { description = "Tags communs";       type = map(string); default = { env = "dev", owner = "gha-terraform" } }
+variable "project" {
+  description = "Nom court du projet"
+  type        = string
+  default     = "demo"
+}
+
+variable "location" {
+  description = "R?gion Azure"
+  type        = string
+  default     = "westeurope"
+}
+
+variable "tags" {
+  description = "Tags communs"
+  type        = map(string)
+  default     = {
+    env   = "dev"
+    owner = "gha-terraform"
+  }
+}
